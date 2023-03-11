fe76fe110d86c0b72753d9900573da6159e3810d
DeanBurke <deanburke07@hotmail.com>
2023-03-11 14:39:51 +0000

    Added initial question regarding department and validated

--- run.py
+++ run.py
@@ -12,8 +12,37 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('company-survey')
 
-results = SHEET.worksheet('results')
 
-data = results.get_all_values()
+def get_employee_dept():
+    """
+    Gets the departments which the employee
+    works under
+    """
+    while True:
+        print("Welcome to our comppany survey.\n")
+        print("Please state what department you currently work for?")
+        print("(Digital, Finance, HR, Legal, Production)\n")
 
-print(data)
+        dept = input("Enter your department here: \n")
+        
+        if validate_dept(dept):
+            print("Data is valid")
+            break
+
+
+def validate_dept(dept):
+    """
+    Checks to make sure user entered a valid department.
+    Responds with an error if not
+    """
+    all_depts = ["digital", "finance", "hr", "legal", "production"]
+
+    if dept.lower() in all_depts:
+        print("Thank you for your selection.\n")
+        return True
+    else:
+        print("Incorrect department, please choose a relevant department.\n")
+        return False
+
+
+get_employee_dept()
