3371f555d1f6c510542b32b5bec8689987bd65b5
DeanBurke <deanburke07@hotmail.com>
2023-03-11 16:28:00 +0000

    Added question one function. and initial validation

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('company-survey')
 
+QUESTIONS = SHEET.worksheet('info')
+
 
 def get_employee_dept():
     """
@@ -25,7 +27,6 @@ def get_employee_dept():
         dept = input("Enter your department here: \n")
 
         if validate_dept(dept):
-            print("Data is valid")
             break
 
     return dept
@@ -39,10 +40,42 @@ def validate_dept(dept):
     all_depts = ["digital", "finance", "hr", "legal", "production"]
 
     if dept.lower() in all_depts:
-        print("Thank you for your selection.\n")
+        print("\nThank you for your selection.\n")
         return True
     else:
-        print("Incorrect department, please choose a relevant department.\n")
+        print("\nIncorrect department, please choose a relevant department.\n")
+        return False
+
+
+def question_one():
+    """
+    Gets the employee's rating to question one from the spreadsheet
+    """
+    print("Please answer the following questions with a rating of 1-5")
+    print("1 being strongly disagree(negative), 5 being strongly agree(positive).\n")
+
+    question = QUESTIONS.cell(2,2).value
+    q_one = input(f"{question}:\n")
+    validate_answer(q_one)
+
+
+def validate_answer(values):
+    """
+    Checks to make sure answer is between 1 and 5
+    Raises ValueError if answer cannot be converted into int,
+    or if there isnt't exactly 1 value.
+    """ 
+    try:
+        if len(values) > 1:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+        value = int(values)
+        if value not in range(1, 6):
+            raise ValueError("Answer must be between 1 and 5")
+        return True
+    except ValueError as e:
+        print(f"Invalid input: {e}")
         return False
 
 
@@ -50,7 +83,8 @@ def main():
     """
     Run all program functions
     """
-    user_dept = get_employee_dept()
+    # user_dept = get_employee_dept()
+    question_one()
 
 
 print("Welcome to our company survey.\n")
